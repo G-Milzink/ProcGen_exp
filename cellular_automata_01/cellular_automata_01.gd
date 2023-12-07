5c0f07b154549c537170be4f89471d957a5609d5
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-12-07 17:50:36 +0100

    add check for map boundries during iteration
    Tiles outside of the map are now considered filled

--- cellular_automata_01/cellular_automata_01.gd
+++ cellular_automata_01/cellular_automata_01.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+#region Node References
 @onready var tile_map = $TileMap
 @onready var noise_gen = $NoiseGen
 @onready var iterations_display = $CanvasLayer/MarginContainer/IterationsDisplay
@@ -8,22 +9,25 @@ extends Node2D
 @onready var tile_chance_display = $CanvasLayer/MarginContainer/TileChanceDisplay
 @onready var neighbours_display = $CanvasLayer/MarginContainer/NeighboursDisplay
 @onready var noise_freq_display = $CanvasLayer/MarginContainer/NoiseFreqDisplay
+#endregion
 
-
+#region Exported Variables
 @export var width = 4
 @export var height = 4
 @export_range(05,95) var noise_cutoff_point = 50
 @export var min_nr_neighbours = 4
 @export var nr_of_iterations = 0
 @export var noise_frequency = 0.2
+#endregion
 
+#region Function Variables
 var noise_grid: PackedVector2Array
 var iterated_grid: PackedVector2Array
 var tile_grid: PackedVector2Array
 var iterations_done = 0
-
 var offset_x
 var offset_y
+#endregion
 
 func _ready():
 	offset_x = -width/2
@@ -32,8 +36,6 @@ func _ready():
 	_generateNoiseGrid()
 	for i in nr_of_iterations:
 		_iterateGrid()
-
-
 
 func _process(delta):
 	_updateDisplay()
@@ -64,34 +66,57 @@ func _iterateGrid():
 	iterated_grid.clear()
 	for x in width:
 		for y in height:
-			tile = (Vector2(x+offset_x, y+offset_y))
+			
 			nr_of_neighbours = 0
+			tile = (Vector2(x+offset_x, y+offset_y))
+
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_TOP_SIDE)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.y < offset_y\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_TOP_RIGHT_CORNER)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.y < offset_y\
+			or neighbour.x > width + offset_x\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_RIGHT_SIDE)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.x > width + offset_x\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.x > width + offset_x\
+			or neighbour.y > height + offset_y\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.y > height + offset_y\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_BOTTOM_LEFT_CORNER)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.y > height + offset_y\
+			or neighbour.x < offset_x\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_LEFT_SIDE)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.x < offset_x\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			neighbour = tile_map.get_neighbor_cell(tile,TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER)
-			if tile_map.get_cell_tile_data(1,neighbour) != null:
+			if neighbour.x < offset_x\
+			or neighbour.y < offset_y\
+			or tile_map.get_cell_tile_data(1,neighbour) != null:
 				nr_of_neighbours += 1
+			
 			if nr_of_neighbours >= min_nr_neighbours:
 				iterated_grid.append(Vector2(tile))
+	
 	tile_map.clear_layer(1)
 	tile_map.set_cells_terrain_connect(1,iterated_grid,0,0,false)
 	iterations_done += 1
@@ -121,7 +146,6 @@ func _updateDisplay():
 	noise_freq_display.add_text("Noise Frequency: ")
 	noise_freq_display.add_text(str(noise_frequency))
 
-
 func _on_regenerate_pressed():
 	iterations_done = 0
 	offset_x = -width/2
@@ -138,7 +162,6 @@ func _on_tile_chance_slider_value_changed(value):
 
 func _on_width_slider_value_changed(value):
 	width = value
-
 
 func _on_height_slider_value_changed(value):
 	height = value
