a816d02ffc9b94439e372c7e535bd8211a765f0e
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-12-08 02:43:14 +0100

    disabled chunk unloading, optimized several systems. InfBiomes now runs acceptibly

--- infinite_biomes/infinite_biomes.gd
+++ infinite_biomes/infinite_biomes.gd
@@ -1,50 +1,55 @@
 extends Node2D
 
 @onready var tile_map = $TileMap
-@onready var noise_gen = $NoiseGens
 @onready var player = $IB_Player
 
-@export var width = 32
-@export var height = 32
+@export var width = 64
+@export var height = 64
 @export var sea_level = 0
 
+@export var moisture: FastNoiseLite
+@export var tempurature: FastNoiseLite
+@export var altitude: FastNoiseLite
 var loaded_chunks = []
 var player_tile_pos
+
+func _ready():
+	moisture.seed = randi()
+	tempurature.seed = randi()
+	altitude.seed = randi()
 
 func _process(delta):
 	player_tile_pos = tile_map.local_to_map(player.position)
 	
 	_generate_chunk(player_tile_pos)
-	_unloadDistantChunks(player_tile_pos)
+	#_unloadDistantChunks(player_tile_pos)
 
 func _generate_chunk(player_position):
-	for x in width:
-		for y in height:
+	var chunk = []
+	for x in range(width):
+		for y in range(height):
 			
 			var pos = Vector2(player_position.x - (width/2.0) + x, player_position.y - (height/2.0) + y)
-			var moist = noise_gen._getMoisture(pos) * 10.0
-			var temp = noise_gen._getTemperature(pos) * 10.0
-			var alt = noise_gen._getAltitude(pos) * 10.0
-			var atlas_coords = Vector2((moist + 10) / 20, (temp + 10) / 20)
-			
+			var moist = ((moisture.get_noise_2d(pos.x,pos.y) +1) * 0.5)*3
+			var temp = ((tempurature.get_noise_2d(pos.x,pos.y) +1) * 0.5)*3
+			var alt = altitude.get_noise_2d(pos.x,pos.y) * 10.0
+			var atlas_coords = Vector2(floor(moist), floor(temp))
 			if alt < sea_level:
-				tile_map.set_cell(0,pos,0, Vector2(3,3))
+				tile_map.set_cell(0,pos,0, Vector2(3,1))
 			else:
 				tile_map.set_cell(0,pos,0, atlas_coords)
-			
-			if Vector2(player_position.x,player_position.y) not in loaded_chunks:
-				loaded_chunks.append(Vector2i(player_position.x,player_position.y))
+			loaded_chunks.append(Vector2i(player_position.x,player_position.y))
 
 func _getDistance(pos1, pos2):
 	var result = pos1 - pos2
 	return sqrt(result.x ** 2.0 + result.y ** 2.0)
 
 func _clearChunk(player_position):
-	for x in width:
-		for y in height:
+	for x in range(width):
+		for y in range(height):
 			
 			var pos = Vector2(player_position.x - (width/2.0) + x, player_position.y - (height/2) + y)
-			tile_map.set_cell(0,pos,-1, Vector2(-1,-1))
+			tile_map.erase_cell(0,pos)
 
 func _unloadDistantChunks(player_position):
 	var unload_distance = (width * 2.0) + 1.0
--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var cellular_automata_01 = preload("res://cellular_automata_01/cellular_automata_01.tscn")
+var cellular_automata = preload("res://cellular_automata_01/cellular_automata_01.tscn")
+var infinite_biomes = preload("res://infinite_biomes/infinite_biomes.tscn")
 
 @onready var menu = $CanvasLayer/MarginContainer/Menu
 @onready var back = $CanvasLayer/MarginContainer2/Control/Back
@@ -19,7 +20,14 @@ func _on_back_pressed():
 		get_tree().quit()
 
 func _on_cel_auto_01_pressed():
-	instance = cellular_automata_01.instantiate()
+	instance = cellular_automata.instantiate()
+	call_deferred("add_sibling", instance)
+	menu.visible = false
+	back.set_text("back")
+
+
+func _on_inf_biomes_pressed():
+	instance = infinite_biomes.instantiate()
 	call_deferred("add_sibling", instance)
 	menu.visible = false
 	back.set_text("back")
