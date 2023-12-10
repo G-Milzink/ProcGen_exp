9f89441560ae9045073bc723ed9fe8c7610e612e
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-12-10 19:08:30 +0100

    complete rework of temp/humidity system: increased granularity of zone type

--- world_generator/world_generator.gd
+++ world_generator/world_generator.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@export var map_width = 1000
-@export var map_height = 1000
+var map_width = 3000
+var map_height = 3000
 
-@export var sea_level = 0.5
-@export var mountain_level = 0.5
-@export var absolute_zero = 0.1
-@export var freezing_point = 0.6
+var sea_level = -0.25
+var mountain_level = 0.5
+var absolute_zero = 0.1
+var freezing_point = 0.6
 
 @export var temperature: GradientTexture2D
 @export var altitude: FastNoiseLite
@@ -20,6 +20,8 @@ var temp_img: Image
 var temp: float
 var alt: float
 var humid: float
+
+var variation: float
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
@@ -44,13 +46,22 @@ func _generateOceans():
 			pos.x = x - map_width/2.0
 			pos.y = y - map_height/2.0
 			# Get map parameters: 
+			variation = noise_map.get_noise_2d(x,y)
+			
 			alt = altitude.get_noise_2d(x,y)
-
+			temp = temp_img.get_pixel(x,y).r + variation * 0.3
+			humid = humidity.get_noise_2d(x,y)
+			
 			if alt < sea_level:
 				tile_map.set_cell(0,pos,0,Vector2(0,0))
 				tile.x = clamp(-(ceil((alt*-4)*2)-3)+3, 0.0, 3.0)
-				tile.y = 0
+				tile.y = 8
 				tile_map.set_cell(0,pos,0,tile)
 			elif alt > mountain_level:
-				print((((alt-0.5)*8)/3)*4)
-				tile_map.set_cell(0,pos,0,Vector2(4,0))
+				tile.x = clamp(round((((alt-0.5)*8)/3)*4)+4, 4.0, 7.0)
+				tile.y = 8
+				tile_map.set_cell(0,pos,0,tile)
+			else:
+					tile.x = clamp(floor((humid+1.0*0.5)*8),0.0,7.0)
+					tile.y = temp*7
+					tile_map.set_cell(0,pos,0,tile)
