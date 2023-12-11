f4fe7169462cf43fdd4ac0f78e4738a8ae0a83e6
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-12-11 23:53:49 +0100

    KCC_DunGen_implement tile placement and room carving

--- KCC_rnd_Dungeon/kcc_rnd_dungeon.gd
+++ KCC_rnd_Dungeon/kcc_rnd_dungeon.gd
@@ -1,66 +1,70 @@
 extends Node2D
 
-var room = preload("res://KCC_rnd_Dungeon/room.tscn")
-
 @export var tile_size = 32
-@export var num_rooms = 50
-@export var min_size = 4
-@export var max_size = 10
-@export var horiz_spread = 100
-@export var cull = 0.4
+@export var num_rooms = 30
+@export var min_size = 6
+@export var max_size = 12
+@export var horiz_spread = 400
+@export var vertic_spread = 200
+@export var cull = .2
 
+var room = preload("res://KCC_rnd_Dungeon/room.tscn")
 var path  #Astar pathfinding object
+
+@onready var tile_map = $TileMap
 
 func _ready():
 	randomize()
 	_makeRooms()
 
-func _makeRooms():
-	for i in num_rooms: 
-		var pos = Vector2(randi_range(-horiz_spread,horiz_spread),0)
-		var r = room.instantiate()
-		var w = min_size + randi() % (max_size - min_size)
-		var h = min_size + randi() % (max_size - min_size)
-		r.makeRoom(pos, Vector2(w,h) * tile_size)
-		$Rooms.add_child(r)
-	# wait for movement to stop:
-	await(get_tree().create_timer(1.5).timeout)
-	# cull rooms:
-	var room_positions = []
-	for room in $Rooms.get_children():
-		if randf() < cull:
-			room.queue_free()
-		else:
-			room.set_freeze_enabled(true)
-			room_positions.append(Vector2(room.position))
-	await get_tree().process_frame
-	# generate a Minimum Spanning Tree connecting rooms
-	path = find_mst(room_positions)
-
 func _draw():
-	for room in $Rooms.get_children():
-		draw_rect(Rect2(room.position - room.size + room.size / 2, room.size ), Color.YELLOW, false)
+	for i in $Rooms.get_children():
+		draw_rect(Rect2(i.position - i.size + i.size / 2, i.size ), Color.YELLOW, false)
 	
 	if path:
 		for p in path.get_point_ids():
 			for c in path.get_point_connections(p):
 				var pp = path.get_point_position(p)
 				var cp = path.get_point_position(c)
-				draw_line(pp,cp,Color.YELLOW, 15, true)
-
-func _process(delta):
-	queue_redraw()
+				draw_line(pp,cp,Color.ORANGE, 15, true)
 
 func _input(event):
 	if event.is_action_pressed("ui_select"):
 		for n in $Rooms.get_children():
 			n.queue_free()
 		_makeRooms()
+	if event.is_action_pressed("ui_focus_next"):
+		make_map()
+
+func _process(delta):
+	queue_redraw()
+
+func _makeRooms():
+	for i in num_rooms: 
+		var pos = Vector2(randi_range(-horiz_spread,horiz_spread),randi_range(-vertic_spread,vertic_spread))
+		var r = room.instantiate()
+		var w = min_size + randi() % (max_size - min_size) * 2
+		var h = min_size + randi() % (max_size - min_size) * 2
+		r.makeRoom(pos, Vector2(w,h) * tile_size)
+		$Rooms.add_child(r)
+	# wait for movement to stop:
+	await(get_tree().create_timer(1.5).timeout)
+	# cull rooms:
+	var room_positions = []
+	for i in $Rooms.get_children():
+		if randf() < cull:
+			i.queue_free()
+		else:
+			i.set_freeze_enabled(true)
+			room_positions.append(Vector2(i.position))
+	await get_tree().process_frame
+	# generate a Minimum Spanning Tree connecting rooms
+	path = find_mst(room_positions)
 
 func find_mst(nodes):
 	#Prims alghorithm
-	var path = AStar2D.new()
-	path.add_point(path.get_available_point_id(),nodes.pop_front())
+	var _path = AStar2D.new()
+	_path.add_point(_path.get_available_point_id(),nodes.pop_front())
 	
 	# repeat until no more nodes remain
 	while nodes:
@@ -68,16 +72,49 @@ func find_mst(nodes):
 		var min_pos = null # position of that node
 		var cur_pos = null # current position
 		# loop through points in path
-		for point1 in path.get_point_ids():
-			var p1_pos = path.get_point_position(point1)
+		for point1 in _path.get_point_ids():
+			var p1_pos = _path.get_point_position(point1)
 			# loop through the remaining nodes
 			for point2 in nodes:
 				if p1_pos.distance_to(point2) < min_dist:
 					min_dist = p1_pos.distance_to(point2)
 					min_pos = point2
 					cur_pos = p1_pos
-		var node = path.get_available_point_id()
-		path.add_point(node,min_pos)
-		path.connect_points(path.get_closest_point(cur_pos),node)
+		var node = _path.get_available_point_id()
+		_path.add_point(node,min_pos)
+		_path.connect_points(_path.get_closest_point(cur_pos),node)
 		nodes.erase(min_pos)
-	return path
+	return _path
+
+func make_map():
+	#create tilemap from generated rooms and path
+	tile_map.clear()
+	
+	# fill tilemap with walls, then carve empty rooms
+	var full_rect = Rect2()
+	
+	for i in $Rooms.get_children():
+		var r = Rect2(i.position-i.size, i.get_node("CollisionShape2D").shape.size*2)
+		full_rect = full_rect.merge(r)
+	var top_left = tile_map.local_to_map(full_rect.position)
+	var bottom_right = tile_map.local_to_map(full_rect.end)
+	for x in range(top_left.x, bottom_right.x):
+		for y in range(top_left.y, bottom_right.y):
+			tile_map.set_cell(0,Vector2(x,y),0,Vector2(1,0))
+	
+	# carve rooms
+	
+	for i in $Rooms.get_children():
+		var s = (i.size / tile_size).floor()
+		var pos = tile_map.local_to_map(i.position)
+		var ul = (i.position / tile_size).floor() - s
+		for x in range(s.x*0.5+1, s.x+s.x*0.5-0.5):
+			for y in range(s.y*0.5+2, s.y+s.y*0.5):
+				tile_map.set_cell(0, Vector2i(ul.x + x, ul.y + y), 0, Vector2i(0, 0))
+		
+		
+		
+
+
+
+
